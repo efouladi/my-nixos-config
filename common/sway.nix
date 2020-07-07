c04eb46ecb668cd1d4ce91487cc0137076773999
Shayan Fouladi <shafo@posteo.net>
2020-07-07 23:08:24 +0200

    Setup sway environment variables

--- common/sway.nix
+++ common/sway.nix
@@ -7,6 +7,19 @@ in
   nixpkgs.overlays = [ waylandOverlay ];
   programs.sway = {
     enable = true;
+    extraSessionCommands = ''
+      export SDL_VIDEODRIVER=wayland
+      # needs qt5.qtwayland in systemPackages
+      export QT_QPA_PLATFORM=wayland
+      export QT_WAYLAND_DISABLE_WINDOWDECORATION="1"
+      # Fix for some Java AWT applications (e.g. Android Studio),
+      # use this if they aren't displayed properly:
+      export _JAVA_AWT_WM_NONREPARENTING=1
+      export MOZ_ENABLE_WAYLAND=1
+      export WLR_DRM_NO_MODIFIERS=1
+      export XDG_CURRENT_DESKTOP=sway
+      export XDG_SESSION_TYPE=wayland
+    '';
     wrapperFeatures.gtk = true;
     extraPackages = with pkgs; [
       swaylock # lockscreen
